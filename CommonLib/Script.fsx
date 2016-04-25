363f6181893a7a2678d8c0d80f602d95cd51e0d5
Cyclic3 <harlan.connor@gmail.com>
2016-04-25 13:17:35 +0100

    Finished transfer of pco

--- CommonLib/Script.fsx
+++ CommonLib/Script.fsx
@@ -1,6 +1,5 @@
-﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
-// for more guidance on F# programming.
-#load "Component1.fs"
-
-open CommonLib
-
+﻿open Microsoft.FSharp.Quotations
+#r "bin/Debug/CommonLib.dll"
+open C_Omega.Delta.Processes
+System.IO.File.WriteAllBytes("/home/harlan/C-Omega/1.tmp",ProcessCreationOptions.Mash <@ {channel = printfn "%A";name = "test";get_state = (fun() -> Sig.START); on_create = ignore} @> )
+ProcessCreationOptions.UnMash(System.IO.File.ReadAllBytes("/home/harlan/C-Omega/1.tmp"),[||])
